ceb76759020b89fb85b42f76c68f007f3d11f6a3
Waniss Boucetta <wanissboucetta@MacBook-Pro-di-Waniss.local>
2025-07-14 15:53:38 +0200

    Aggiornata favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        {/* Puoi anche usare favicon.ico se preferisci */}
+      </Head>
       <body>
         <Main />
         <NextScript />
